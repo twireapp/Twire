52d41972ba98abea86364b58e7326bb05990068f
Sebastian Rask Jepsen <sebofdoom@gmail.com>
2017-11-02 20:07:53 +0100

    Created SecretKeys.java for development

---
+++ app/src/main/java/com/sebastianrask/bettersubscription/misc/SecretKeys.java
@@ -0,0 +1,13 @@
+package com.sebastianrask.bettersubscription.misc;
+
+/**
+ * Created by sebastian on 24/07/2017.
+    Add Keys
+ */
+
+public class SecretKeys {
+    public static final String TWITCH_CLIENT_ID = "s9r26uney64y60m9g267600jb6n2lr";
+    public static final String CHROME_CAST_APPLICATION_ID = "";
+    public static final String IN_APP_PURCHASE_BASE64_KEY = ""; 
+    public static final String NO_LOG_IN_ACCESS_TOKEN = "";
+}
