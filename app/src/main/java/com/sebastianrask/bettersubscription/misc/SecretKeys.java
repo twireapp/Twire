14ba5f1cd47cbd05162573c62e673833eda8de14
Sebastian Rask Jepsen <sebastianraskjepsen@gmail.com>
2017-11-05 13:25:38 +0100

    Re-added SecretKeys

---
+++ app/src/main/java/com/sebastianrask/bettersubscription/misc/SecretKeys.java
@@ -0,0 +1,13 @@
+package com.sebastianrask.bettersubscription.misc;
+
+/**
+ * Created by Sebastian on 24/07/2017.
+ Add Keys
+ */
+
+public class SecretKeys {
+    public static final String TWITCH_CLIENT_ID = "s9r26uney64y60m9g267600jb6n2lr"; // Development Key
+    public static final String CHROME_CAST_APPLICATION_ID = "CC1AD845"; // Dummy key. App host ed by google
+    public static final String IN_APP_PURCHASE_BASE64_KEY = "";
+    public static final String NO_LOG_IN_ACCESS_TOKEN = ""; // Find access token to use if you want chat to work without users logging in
+} 
