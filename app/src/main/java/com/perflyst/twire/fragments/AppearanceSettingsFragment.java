430d60d5ed738a9b4722629ba870ff2728c4f091
Perflyst <mail@perflyst.de>
2019-03-25 18:25:28 +0100

    Remove unused strings and drop useless permissions

--- app/src/main/java/com/perflyst/twire/fragments/AppearanceSettingsFragment.java
+++ app/src/main/java/com/perflyst/twire/fragments/AppearanceSettingsFragment.java
@@ -162,7 +162,6 @@ public class AppearanceSettingsFragment extends Fragment {
 		DialogService.getChooseStreamCardStyleDialog(getActivity(), new LayoutSelector.OnLayoutSelected() {
 			@Override
 			public void onSelected(String title, int index, View previewView) {
-				track(R.string.action_stream_style, title);
 
 				View sharedPadding = previewView.findViewById(R.id.shared_padding);
 				ImageView view = (ImageView) previewView.findViewById(R.id.image_stream_preview);
@@ -195,7 +194,6 @@ public class AppearanceSettingsFragment extends Fragment {
 		DialogService.getChooseGameCardStyleDialog(getActivity(), new LayoutSelector.OnLayoutSelected() {
 			@Override
 			public void onSelected(String title, int index, View previewView) {
-				track(R.string.action_game_style, title);
 
 				View sharedPadding = previewView.findViewById(R.id.shared_padding);
 				ImageView view = (ImageView) previewView.findViewById(R.id.image_game_preview);
@@ -238,7 +236,6 @@ public class AppearanceSettingsFragment extends Fragment {
 		DialogService.getChooseStreamerCardStyleDialog(getActivity(), new LayoutSelector.OnLayoutSelected() {
 			@Override
 			public void onSelected(String title, int index, View previewView) {
-				track(R.string.action_streamer_style, title);
 
 				View nameView = previewView.findViewById(R.id.displayName);
 				ImageView streamerLogo = (ImageView) previewView.findViewById(R.id.profileLogoImageView);
@@ -275,7 +272,6 @@ public class AppearanceSettingsFragment extends Fragment {
 				new MaterialDialog.ListCallbackSingleChoice() {
 					@Override
 					public boolean onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
-						track(R.string.action_stream_size, text.toString());
 						settings.setAppearanceStreamSize(text.toString());
 						initSummaries();
 						return true;
@@ -292,7 +288,6 @@ public class AppearanceSettingsFragment extends Fragment {
 				new MaterialDialog.ListCallbackSingleChoice() {
 					@Override
 					public boolean onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
-						track(R.string.action_streamer_size, text.toString());
 
 						settings.setAppearanceChannelSize(text.toString());
 						initSummaries();
@@ -310,8 +305,6 @@ public class AppearanceSettingsFragment extends Fragment {
 				new MaterialDialog.ListCallbackSingleChoice() {
 					@Override
 					public boolean onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
-						track(R.string.action_game_size, text.toString());
-
 						settings.setAppearanceGameSize(text.toString());
 						initSummaries();
 						return true;
