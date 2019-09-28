5c4a23830f0688215696c6d57f5e5d23b5c70bb7
samfun123 <samdinner@hotmail.com>
2019-09-28 01:51:01 -0400

    Make it so you can swipe to open VOD chat like regular chat

--- app/src/main/java/com/perflyst/twire/fragments/StreamFragment.java
+++ app/src/main/java/com/perflyst/twire/fragments/StreamFragment.java
@@ -778,7 +778,7 @@ public class StreamFragment extends Fragment {
     }
 
     private void setupLandscapeChat() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && vodId == null && settings.isChatLandscapeSwipable() && settings.isChatInLandscapeEnabled()) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 && settings.isChatLandscapeSwipable() && settings.isChatInLandscapeEnabled()) {
             final int width = getScreenWidth(getActivity());
 
             View.OnTouchListener touchListener = new View.OnTouchListener() {
