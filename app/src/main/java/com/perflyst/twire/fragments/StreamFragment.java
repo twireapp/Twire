twireapp/Twire

commit 622a5a95c446d0f30fddbe9a69574ad1a18b7faf

Fix the stream rewinding when opening the app from PiP
Closes #112

app/src/main/java/com/perflyst/twire/fragments/StreamFragment.java
@@ -632,8 +632,6 @@ public class StreamFragment extends Fragment implements Player.EventListener, Pl
             showVideoInterface();
             updateUI();
         }
-
-        player.seekTo(currentProgress);
     }
 
     @Override

commit 47a83d0bb936caaa5ddea4108793a128c94d5a89

Fix streams rewinding when resuming
Caused by 622a5a95c446d0f30fddbe9a69574ad1a18b7faf

app/src/main/java/com/perflyst/twire/fragments/StreamFragment.java
@@ -194,8 +194,8 @@ public class StreamFragment extends Fragment implements Player.EventListener, Pl
     private Integer triesForNextBest = 0;
 
     private static int totalVerticalInset;
-    private boolean pictureInPictureEnabled;
-    private static boolean pipDisabling;
+    private boolean pictureInPictureEnabled; // Tracks if PIP is enabled including the animation.
+    private static boolean pipDisabling; // Tracks the PIP disabling animation.
     private MediaSessionCompat mediaSession;
 
     public static StreamFragment newInstance(Bundle args) {
@@ -612,6 +612,11 @@ public class StreamFragment extends Fragment implements Player.EventListener, Pl
     @Override
     public void onResume() {
         super.onResume();
+
+        // If the app was closed in the background we need to seek to currentProgress when resuming.
+        // Android also triggers onResume when coming out of PIP but we don't need to do it then.
+        if (!pipDisabling)
+            player.seekTo(currentProgress);
 
         pipDisabling = false;
 