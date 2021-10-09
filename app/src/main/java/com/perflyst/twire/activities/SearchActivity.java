d5b3c97599491373e2d30879119a38b59661a8e2
cdhiraj40 <chauhandhiraj40@gmail.com>
2021-10-09 10:54:07 +0530

    Closing keyboard when clicked in the background

--- app/src/main/java/com/perflyst/twire/activities/SearchActivity.java
+++ app/src/main/java/com/perflyst/twire/activities/SearchActivity.java
@@ -1,12 +1,16 @@
 package com.perflyst.twire.activities;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -425,5 +429,22 @@ public class SearchActivity extends ThemeActivity {
         public int getItemCount() {
             return TOTAL_COUNT;
         }
+    }
+    //hides keyboard in all the fragments when clicked outside of activity
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
