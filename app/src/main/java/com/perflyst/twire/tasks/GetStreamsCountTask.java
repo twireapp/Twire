3a1684851a9ccf94757efdb861886fb0d354fe9e
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-12-17 15:44:49 +0100

    loop over every channel in the Database

--- app/src/main/java/com/perflyst/twire/tasks/GetStreamsCountTask.java
+++ app/src/main/java/com/perflyst/twire/tasks/GetStreamsCountTask.java
@@ -7,7 +7,10 @@ import com.perflyst.twire.model.ChannelInfo;
 import com.perflyst.twire.service.Service;
 import com.perflyst.twire.service.Settings;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sebastian Rask on 26-06-2016.
@@ -15,9 +18,11 @@ import org.json.JSONObject;
 public class GetStreamsCountTask extends AsyncTask<Void, Void, Integer> {
     private final Settings settings;
     private final Delegate delegate;
+    private final Context context;
 
     public GetStreamsCountTask(Context context, Delegate delegate) {
         this.settings = new Settings(context);
+        this.context = context;
         this.delegate = delegate;
     }
 
@@ -25,24 +30,60 @@ public class GetStreamsCountTask extends AsyncTask<Void, Void, Integer> {
     protected Integer doInBackground(Void... params) {
         try {
             // build the api link
-            String kraken_url = "https://api.twitch.tv/kraken/streams?limit=100&stream_type=live&offset=0&channel=";
+            String helix_url = "https://api.twitch.tv/helix/streams";
             String user_logins = "";
 
             GetFollowsFromDB subscriptionsTask = new GetFollowsFromDB();
             subscriptionsTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, settings.getContext());
 
+            ArrayList<String> requesturls = new ArrayList<>();
+            boolean first_id = true;
+            int number = 0;
+            int exactnumber = 0;
+
+            // loop over all channel in the DB
             for (ChannelInfo si : subscriptionsTask.get().values()) {
-                user_logins = user_logins + si.getUserId() + ",";
+                // if the number of channels, already in the url, is smaller than 99 and is not the last channel
+                // e.g. if there are 160 Channels in the DB then this will result in 2 request urls ([0-99] and [100-159])
+                if (number <= 99 && exactnumber != subscriptionsTask.get().values().size() -1) {
+                    if (first_id) {
+                        // if this is the first id then use ?
+                        user_logins = "?user_id=" + si.getUserId();
+                        first_id = false;
+                    } else {
+                        // not the first id use &
+                        user_logins = user_logins + "&user_id=" + si.getUserId();
+                    }
+                    number++;
+                    // if the request url has 100 user ids or is the last channel in the list
+                } else if (number > 99 || exactnumber == (subscriptionsTask.get().values().size() -1)) {
+                    // add the new request url to the list
+                    requesturls.add(helix_url + user_logins);
+                    // reset stuff
+                    first_id = true;
+                    user_logins = "";
+                    number = 0;
+                }
+                exactnumber++;
             }
 
-
-            kraken_url = kraken_url + user_logins;
-            final String STREAMS_ARRAY = "streams";
-
-            String jsonString = Service.urlToJSONString(kraken_url);
-            JSONObject fullDataObject = new JSONObject(jsonString);
-
-            return fullDataObject.getJSONArray(STREAMS_ARRAY).length();
+            final String STREAMS_ARRAY = "data";
+            JSONArray final_array = new JSONArray();
+            // for every request url in the list
+            for (int i=0; i<requesturls.size(); i++) {
+                String temp_jsonString;
+                // request the url
+                temp_jsonString = Service.urlToJSONStringHelix(requesturls.get(i), context);
+                JSONObject fullDataObject = new JSONObject(temp_jsonString);
+                // create the array
+                JSONArray temp_array = fullDataObject.getJSONArray(STREAMS_ARRAY);
+                // append the new array to the final one
+                for (int x=0; x<temp_array.length(); x++) {
+                    final_array.put(temp_array.get(x));
+                }
+            }
+            // return the length of the array
+            return final_array.length();
         } catch (Exception e) {
             e.printStackTrace();
         }
