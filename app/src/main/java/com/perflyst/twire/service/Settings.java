24ddefaa5ecaae6685968d9b95ffdf26a25286ca
Fjara <robuhde@gmail.com>
2024-10-12 16:40:42 -0400

    Add play when locked setting
    Fixed a spelling error in Settings.java
    Play stream when locked setting
    Add missing default strings (warnings)

--- app/src/main/java/com/perflyst/twire/service/Settings.java
+++ app/src/main/java/com/perflyst/twire/service/Settings.java
@@ -41,7 +41,8 @@ public class Settings {
     private final String STREAM_PLAYER_SHOW_VIEWERCOUNT = "streamPlayerShowViewerCount",
             STREAM_PLAYER_SHOW_RUNTIME = "streamPlayerShowRuntime",
             STREAM_PLAYER_REVEAL_NAVIGATION = "streamPlayerRevealNavigation",
-            STREAM_PLAYER_AUTO_PLAYBACK = "streamPlayerAutoPlackbackOnReturn",
+            STREAM_PLAYER_AUTO_PLAYBACK = "streamPlayerAutoPlaybackOnReturn",
+            STREAM_PLAYER_LOCKED_PLAYBACK = "streamPlayerLockedPlayback",
             STREAM_PLAYER_TYPE = "streamPlayerType",
             STREAM_PLAYER_PROXY = "streamPlayerProxy";
     private final String APPEARANCE_STREAM_STYLE = "appStreamStyle";
@@ -738,9 +739,19 @@ public class Settings {
         editor.commit();
     }
 
+    public boolean getStreamPlayerAutoContinuePlayback() {
+        SharedPreferences preferences = getPreferences();
+        return preferences.getBoolean(this.STREAM_PLAYER_AUTO_PLAYBACK, false);
+    }
+
     public boolean getStreamPlayerAutoContinuePlaybackOnReturn() {
         SharedPreferences preferences = getPreferences();
         return preferences.getBoolean(this.STREAM_PLAYER_AUTO_PLAYBACK, false);
+    }
+
+    public boolean getStreamPlayerLockedPlayback() {
+        SharedPreferences preferences = getPreferences();
+        return preferences.getBoolean(this.STREAM_PLAYER_LOCKED_PLAYBACK, true);
     }
 
     /**
@@ -780,6 +791,12 @@ public class Settings {
     public void setStreamPlayerAutoContinuePlaybackOnReturn(boolean autoPlayback) {
         SharedPreferences.Editor editor = getEditor();
         editor.putBoolean(this.STREAM_PLAYER_AUTO_PLAYBACK, autoPlayback);
+        editor.commit();
+    }
+
+    public void setStreamPlayerLockedPlayback(boolean lockedPlayback) {
+        SharedPreferences.Editor editor = getEditor();
+        editor.putBoolean(this.STREAM_PLAYER_LOCKED_PLAYBACK, lockedPlayback);
         editor.commit();
     }
 
